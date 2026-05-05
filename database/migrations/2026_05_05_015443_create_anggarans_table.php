c8688e4fbe602ae0e774563c0dc2a3c8b4d715c5
Muhammad-Zacky <zackym038@gmail.com>
2026-05-05 10:14:29 +0700

    feat: Update anggarans migration to remove 'total_anggaran' and 'kategori', add 'original_budget' and 'unreleased_persen'

--- database/migrations/2026_05_05_015443_create_anggarans_table.php
+++ database/migrations/2026_05_05_015443_create_anggarans_table.php
@@ -8,17 +8,19 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void
-    {
-        Schema::create('anggarans', function (Blueprint $table) {
-            $table->id();
-            $table->string('kode_coa')->unique(); // Misal: 5020100000
-            $table->string('nama_item');          // Misal: Exp Bhn Bakar
-            $table->bigInteger('total_anggaran'); // Pagu 1 tahun
-            $table->string('kategori')->default('Umum'); // Untuk menandai 'The Big 4'
-            $table->timestamps();
-        });
-    }
+        {
+            Schema::create('anggarans', function (Blueprint $table) {
+                $table->id();
+                $table->string('kode_coa')->unique();
+                $table->string('nama_item');
 
+                // Logika Baru: Hanya 2 ini yang diinput
+                $table->bigInteger('original_budget');
+                $table->integer('unreleased_persen')->default(0); // Contoh: 30 untuk 30%
+
+                $table->timestamps();
+            });
+        }
     public function down(): void
     {
         Schema::dropIfExists('anggarans');
